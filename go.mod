69f27966a21eed7c17fedcd2bc14dcd41d2912e5
loner <gaoht@mama.cn>
2021-07-15 22:25:28 +0800

    test

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/loner-way/loner-gin-vue
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2 // indirect
+require (
+	github.com/beego/bee v1.12.3 // indirect
+	github.com/gin-gonic/gin v1.7.2 // indirect
+	github.com/shuwenhe/utils v0.0.0-20201024040814-4486ec1b9600 // indirect
+)
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"loner-gin-vue/controller"
+	"github.com/loner-way/loner-gin-vue/controller"
 )
 
 func main()  {
 	 r := gin.Default()
 	 r.GET("/Ping", controller.Ping)
+
+	 // 用户注册
+	r.GET("/api/user/register",controller.Register)
+
 	// listen and serve on 0.0.0.0:8080
 	r.Run()
 }
